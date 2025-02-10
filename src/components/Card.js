b24f58286efbe8d72ab1ffc129c5a6e8a029c27c
siddharth <boorgusiddharth@gmail.com>
2025-02-10 16:58:30 +0530

    fff

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,6 +5,7 @@ const Card = ({item}) => {
     <div>
       <img style={{ width: '150px', height: '200px' }}  src={item?.thumbnail || item?.image}/>
       <div>{item?.title.slice(0,20)}</div>
+      <div>hello</div>
     
     </div>
   )
